d8f0363a06becf46e1516bf0e0471b5efb3849f5
ZikriFarhan <dzikrifarhan1210@gmail.com>
2022-04-22 10:54:56 +0700

    change style

--- src/test/Album.test.js
+++ src/test/Album.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import Albums from "../src/components/component/Album";
-import store from "../src/components/pages/reducer/store";
+import Albums from "../components/component/Album";
+import store from "../components/pages/reducer/store";
 import Provider from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
